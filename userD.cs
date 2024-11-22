674605386311cc401f5d704e725549411df93d3e
mzaid622 <M.Zaid990447@gmail.com>
2024-11-22 11:22:43 +0500

    adding user

---
+++ userD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SE_Project
+{
+    public partial class userD : Form
+    {
+        public userD()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
