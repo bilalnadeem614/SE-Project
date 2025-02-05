17c4280f73125164f387d2989358a3691ad2738e
Bilal Nadeem <84433865+bilalnadeem614@users.noreply.github.com>
2025-02-05 13:12:31 +0500

    Adding WCFservices

---
+++ WcfService2/Service1.svc.cs
@@ -0,0 +1,83 @@
+﻿using BCrypt.Net;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        private string connectionString = "Server=127.0.0.1;Database=test;Uid=root;Pwd=12345;";
+
+        public bool SignUpUser(string name, string email, string password, int role)
+        {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "INSERT INTO users (name, email, password, role) VALUES (@name, @Email, @Password, @Role)";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@Password", hashedPassword);
+                    command.Parameters.AddWithValue("@Role", role);
+
+                    connection.Open();
+                    return command.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        public int LoginUser(string email, string password)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT password, role FROM users WHERE email = @Email";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Email", email);
+                    connection.Open();
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string hashedPassword = reader["password"].ToString();
+                            int role = Convert.ToInt32(reader["role"]);
+                            if (BCrypt.Net.BCrypt.Verify(password, hashedPassword))
+                            {
+                                return role;
+                            }
+                        }
+                    }
+                }
+            }
+            return -1; // Login failed
+        }
+    }
+}
